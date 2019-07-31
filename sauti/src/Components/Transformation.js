3e0bcf7855460c1694790536b8a71cdb04aae21e
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-07-31 13:54:35 -0700

    Drafted JS logic to get gender and cell phone

--- sauti/src/Components/Transformation.js
+++ sauti/src/Components/Transformation.js
@@ -7,13 +7,12 @@ class Transformation extends React.Component {
     this.state = {
       realData: [],
       usersArray: [],
-      dataFromLance: [], 
+      dataFromLance: [],
       distinctUsers: []
     };
   }
 
   componentDidMount() {
-    
     axios
       // For development: ${process.env.REACT_APP_BACKEND_URL}/sessions/products/1
       .get(`https://sa-stage.herokuapp.com/sessions/lance/all`)
@@ -26,6 +25,9 @@ class Transformation extends React.Component {
       })
       .then(res => {
         this.createUsersArray();
+      })
+      .then(res => {
+        this.getGender();
       });
   }
 
@@ -44,20 +46,48 @@ class Transformation extends React.Component {
 
     const distinctUsers = [];
     const map = new Map();
-    for (const item of array) { // for each element of the array that contains duplicates
-      if (!map.has(item.cell_num)) { //if map does not contain an object with the cell_num (userid), it includes it and pushes it to result
+    for (const item of array) {
+      // for each element of the array that contains duplicates
+      if (!map.has(item.cell_num)) {
+        //if map does not contain an object with the cell_num (userid), it includes it and pushes it to result
         map.set(item.cell_num, true);
         distinctUsers.push({
           cell_num: item.cell_num
         });
       }
     }
-    this.setState({...this.state, distinctUsers: distinctUsers})
-    // Console log to test: console.log(distinctUsers);
+    this.setState({ ...this.state, distinctUsers: distinctUsers });
+    //console.log(distinctUsers);
   };
 
-  // Map through array 
-  // Check if sessions with that cell phone number have gender survey answered 
+  // Map over session array
+  // See if session did gender survey
+  // If did gender survey, get the cell phone number
+  // Then map through distinct User array, and append gender to right cell phone number
+  getGender = () => {
+    this.state.realData.map(element => {
+      
+      if (element.data.includes("Male")) {
+        this.state.distinctUsers.map(user => {
+          if (user.cell_num === element.data.cell_num) {
+            user.gender = "Male";
+          }
+        });
+      }
+
+      if (element.data.includes("Female")) {
+        this.state.distinctUsers.map(user => {
+          if (user.cell_num === element.data.cell_num) {
+            user.gender = "Female";
+          }
+        });
+      }
+    });
+
+    let arrayWithGender = this.state.distinctUsers;
+    this.setState({...this.state, distinctUsers: arrayWithGender});
+    console.log(this.state.distinctUsers); 
+  };
 
   render() {
     return (
