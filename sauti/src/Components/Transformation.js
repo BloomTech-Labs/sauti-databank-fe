375cf98b6e96586891c62d42570fb8ed6d6384ff
cashcoder88 <cashglobe@gmail.com>
2019-08-01 14:38:42 -0600

    Added getProduce function to distinctUsers array

--- sauti/src/Components/Transformation.js
+++ sauti/src/Components/Transformation.js
@@ -39,6 +39,9 @@ class Transformation extends React.Component {
         this.getCrossingFreq();
       })
       .then(res => {
+        this.getProduce();
+      })
+      .then(res => {
         this.getLanguage();
       })
       .then(res => {
@@ -72,8 +75,8 @@ class Transformation extends React.Component {
           age: null,//set gender to null inside every object so that every object has a gender property.
           education: null,
           crossing_freq: null,
+          produce: null,
           // primary_income: null,
-          // produce: null,
           language: null,
           country_of_residence: null
         });
@@ -264,30 +267,32 @@ class Transformation extends React.Component {
     console.log("total yes no", totalOtherWay);
   }
 
-  // getPrimaryIncome = () => {
-  //   let arrayWithPrimaryIncome = this.state.distinctUsers;
-
-  //   this.state.realData.map(element => {
-  //     let num = element.cell_num;
-  //     if (element.data.includes("Yes")) {
-  //       arrayWithPrimaryIncome.map(user => {
-  //         if (user.cell_num === num) {
-  //           user.primary_income = "Yes";
-  //         }
-  //       });
-  //     } else if (element.data.includes("No")) {
-  //       arrayWithPrimaryIncome.map(user => {
-  //         if (user.cell_num === num) {
-  //           user.primary_income = "No";
-  //         }
-  //       });
-  //     } 
-  //   });
 
 
-  //   this.setState({ ...this.state, distinctUsers: arrayWithPrimaryIncome });
+  getProduce = () => {
+    let arrayWithProduce = this.state.distinctUsers;
 
-  // };
+    this.state.realData.map(element => {
+      let num = element.cell_num;
+      if (element.data.includes(`survey-2-produce\";a:1:{i:0;s:3`) || element.data.includes(`survey-2-produce\";a:1:{i:0;s:4`))  {
+        arrayWithProduce.map(user => {
+          if (user.cell_num === num) {
+            user.produce = "Yes";
+          }
+        });
+      } else if (element.data.includes(`survey-2-produce\";a:1:{i:0;s:2`)) {
+        arrayWithProduce.map(user => {
+          if (user.cell_num === num) {
+            user.produce = "No";
+          }
+        });
+      } 
+    });
+
+
+    this.setState({ ...this.state, distinctUsers: arrayWithProduce });
+
+  };
 
   
   getLanguage = () => {
