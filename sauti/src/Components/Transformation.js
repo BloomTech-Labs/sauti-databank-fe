af0461e22eca6c33c10b7ba6c049c849ad152359
cashcoder88 <cashglobe@gmail.com>
2019-08-01 15:13:45 -0600

    Added getPrimaryIncome function to distinctUsers array

--- sauti/src/Components/Transformation.js
+++ sauti/src/Components/Transformation.js
@@ -42,6 +42,9 @@ class Transformation extends React.Component {
         this.getProduce();
       })
       .then(res => {
+        this.getPrimaryIncome();
+      })
+      .then(res => {
         this.getLanguage();
       })
       .then(res => {
@@ -76,7 +79,7 @@ class Transformation extends React.Component {
           education: null,
           crossing_freq: null,
           produce: null,
-          // primary_income: null,
+          primary_income: null,
           language: null,
           country_of_residence: null
         });
@@ -294,6 +297,62 @@ class Transformation extends React.Component {
 
   };
 
+  getProduce = () => {
+    let arrayWithProduce = this.state.distinctUsers;
+
+    this.state.realData.map(element => {
+      let num = element.cell_num;
+      if (element.data.includes(`survey-2-produce\";a:1:{i:0;s:3`) || element.data.includes(`survey-2-produce\";a:1:{i:0;s:4`))  {
+        arrayWithProduce.map(user => {
+          if (user.cell_num === num) {
+            user.produce = "Yes";
+          }
+        });
+      } else if (element.data.includes(`survey-2-produce\";a:1:{i:0;s:2`)) {
+        arrayWithProduce.map(user => {
+          if (user.cell_num === num) {
+            user.produce = "No";
+          }
+        });
+      } 
+    });
+
+
+    this.setState({ ...this.state, distinctUsers: arrayWithProduce });
+
+  };
+
+  getPrimaryIncome = () => {
+
+    let arrayWithPrimaryIncome = this.state.distinctUsers;
+
+    this.state.realData.map(element => {
+      let num = element.cell_num;
+      if (element.data.includes(`survey-1-primaryincome\";a:1:{i:0;s:3`) || element.data.includes(`survey-1-primaryincome\";a:1:{i:0;s:4`))  {
+        arrayWithPrimaryIncome.map(user => {
+          if (user.cell_num === num) {
+            user.primary_income = "Yes";
+          }
+        });
+      } else if (element.data.includes(`survey-1-primaryincome\";a:1:{i:0;s:2`)) {
+        arrayWithPrimaryIncome.map(user => {
+          if (user.cell_num === num) {
+            user.primary_income = "No";
+          }
+        });
+      } 
+    });
+
+
+    this.setState({ ...this.state, distinctUsers: arrayWithPrimaryIncome });
+
+  };
+
+ /*
+ survey-1-primaryincome\";a:1:{i:0;s:3:\"Yes\" survey-1-primaryincome\";a:1:{i:0;s:4:\"Ndio\
+
+ "survey-1-primaryincome\";a:1:{i:0;s:2:\"La\" survey-1-primaryincome\";a:1:{i:0;s:2:\"No\
+ */
   
   getLanguage = () => {
     let arrayWithLanguage = this.state.distinctUsers;
