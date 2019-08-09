60220e50ec7349279ba99f4b2bd05ff6d1c465bf
numberoneecsstudent <chrlopez@ucdavis.edu>
2019-08-09 13:21:00 -0700

    created the language, country, and age charts

---
+++ sauti/src/Components/Chart/LanguageChart.js
@@ -0,0 +1,232 @@
+// Importing dependencies
+import React from 'react'; 
+import { ResponsiveBar } from "@nivo/bar"; 
+import axios from 'axios';
+
+// Creating class for Gender Chart so it can hold state and receive props
+class LanguageChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      totalCount: 0,
+      data: [],
+      keys: ["English", "Swahili", "Kinyarwanda", "Luganda", "Lukiga"],
+      color: "nivo",
+      englishPercentage: 0,
+      swahiliPercentage: 0,
+      kinyarwandaPercentage: 0,
+      lugandaPercentage: 0,
+      lukigaPercentage: 0,
+      englishCount: 0,
+      swahiliCount: 0,
+      kinyarwandaCount: 0,
+      lugandaCount: 0,
+      lukigaCount: 0,
+    };
+  }
+
+  componentDidMount() {
+
+    axios
+      .get("https://staging-sauti-labs-14.herokuapp.com/users/all/language/all")
+      .then(res => {
+        //console.log('totalCount', res.data.length)
+        this.setState(
+          {
+            ...this.state,
+            users: res.data,
+            totalCount: res.data.length
+          },
+          () => {
+            this.getLanguageEnglish();
+          }
+        );
+      })
+  }
+
+  getLanguageEnglish = () => {
+    axios
+      .get("https://staging-sauti-labs-14.herokuapp.com/users/all/language/english/count")
+      .then(res => {
+        console.log("english res count", res.data);
+        this.setState({
+          ...this.state,
+          englishCount: res.data
+        },
+        () => {
+          this.getLanguageKinyarwanda();
+        }
+        );
+        console.log(this.state.EnglishCount);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  getLanguageKinyarwanda = () => {
+    axios
+      .get("https://staging-sauti-labs-14.herokuapp.com/users/all/language/kinyarwanda/count")
+      .then(res => {
+        this.setState({
+          ...this.state,
+          kinyarwandaCount: res.data
+        }, () => {
+          this.getLanguageLuganda();
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  getLanguageLuganda = () => {
+    axios
+      .get("https://staging-sauti-labs-14.herokuapp.com/users/all/language/luganda/count")
+      .then(res => {
+        this.setState({
+          ...this.state,
+          lugandaCount: res.data
+        }, () => {
+          this.getLanguageLukiga();
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  getLanguageLukiga = () => {
+    axios
+      .get("https://staging-sauti-labs-14.herokuapp.com/users/all/language/lukiga/count")
+      .then(res => {
+        this.setState({
+          ...this.state,
+          lukigaCount: res.data
+        }, () => {
+          this.setPercentages();
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  
+  setPercentages = () => {
+    const totalCount = this.state.totalCount;
+    // let totalCount = dailyCount + weeklyCount + monthlyCount + neverCount;
+    let englishPercentage = Math.round((this.state.englishCount / totalCount) * 100);
+    let swahiliPercentage = Math.round((this.state.swahiliCount / totalCount) * 100);
+    let kinyarwandaPercentage = Math.round((this.state.kinyarwandaCount / totalCount) * 100);
+    let lugandaPercentage = Math.round((this.state.lugandaCount / totalCount) * 100);
+    this.setState(
+      {
+        ...this.state,
+        englishPercentage: englishPercentage,
+        swahiliPercentage: swahiliPercentage,
+        kinyarwandaPercentage: kinyarwandaPercentage,
+        lugandaPercentage: lugandaPercentage
+      },
+      () => {
+         this.setState({
+          ...this.state,
+          data: [
+            {
+              Language: "English",
+              English: this.state.englishPercentage,
+              EnglishColor: "hsl(65, 70%, 50%)"
+            },
+            {
+              Language: "Swahili",
+              Swahili: this.state.swahiliPercentage,
+              SwahiliColor: "hsl(65, 70%, 50%)"
+            },
+            {
+              Language: "Kinyarwanda",
+              Kinyarwanda: this.state.kinyarwandaPercentage,
+              SwahiliColor: "hsl(65, 70%, 50%)"
+            },
+            {
+              Language: "Luganda",
+              Luganda: this.state.lugandaPercentage,
+              LugandaColor: "hsl(65, 70%, 50%)"
+            },
+            {
+              Language: "Lukiga",
+              Lukiga: this.state.lukigaPercentage,
+              LukigaColor: "hsl(65, 70%, 50%)"
+            },
+          ]
+        });
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="Chart">
+        <h2>Language</h2>
+        <ResponsiveBar
+          data={this.state.data} // Data needed
+          keys={this.state.keys} // Values to display in Y axis
+          indexBy="Language"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
+          groupMode="stacked"
+          colors={{ scheme: this.state.color }}
+          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          maxValue={100}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Language",
+            legendPosition: "middle",
+            legendOffset: 30
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "%",
+            legendPosition: "middle",
+            legendOffset: -40
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: "left-to-right",
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemOpacity: 1
+                  }
+                }
+              ]
+            }
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+        />
+      </div>
+    );
+  }
+}
+
+export default LanguageChart;
