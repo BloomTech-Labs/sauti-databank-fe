57b31d3600e766eae8b242a305fd450f41d7f387
cashcoder88 <cashglobe@gmail.com>
2019-08-15 14:47:20 -0600

    Changed Language Chart Hover

--- sauti/src/Components/Chart/GenderChart.js
+++ sauti/src/Components/Chart/GenderChart.js
@@ -194,30 +194,6 @@ class GenderChart extends React.Component {
           labelSkipHeight={0}
           labelTextColor="black"
           theme={theme}
-          legends={[
-            {
-              dataFrom: "keys",
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 120,
-              translateY: 0,
-              itemsSpacing: 2,
-              itemWidth: 100,
-              itemHeight: 20,
-              itemDirection: "left-to-right",
-              itemOpacity: 0.85,
-              symbolSize: 20,
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemOpacity: 1
-                  }
-                }
-              ]
-            }
-          ]}
           tooltip={({ id, value}) => (
             <strong style={{color: "#000000", fontSize: "15px", fontFamily: "Helvetica"}}>
                 {id}: {value}%
