f4480b391743eb351819636af860e20cef067d7b
Robert <rc3331@gmail.com>
2019-12-16 10:40:20 -0600

    committing initial setup of apollo on react client

--- sauti/src/index.js
+++ sauti/src/index.js
@@ -4,9 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({ 
+    uri: "http://localhost:4000/graphql"
+  });  
+
 ReactDOM.render(
-  <Router>
+<ApolloProvider client={client}>
     <App />
-  </Router>,
-  document.getElementById("root")
-);
+</ApolloProvider>
+, document.getElementById('root'));
