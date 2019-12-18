f1070542d903d2c13208a2fd347a1aa87eb4974e
Robert <rc3331@gmail.com>
2019-12-18 17:12:55 -0600

    changing wording in dataparse this branch currently crashing

---
+++ sauti/src/Components/graphLabels.js
@@ -0,0 +1,13 @@
+const dataOrder ={
+    education: {
+    labels: ['No formal education', 'Primary', 'Secondary', 'University/College'],
+    structure: [{"education": "No formal education", "education": "Primary", "education": "Secondary", "education": "University/College"}]
+    },
+    gender: {
+        fasdj: "sdfa"
+    }
+
+}
+
+
+export default dataOrder
