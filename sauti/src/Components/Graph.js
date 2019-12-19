3f2b4244560bb459615963ec551ef8f2c23f2262
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-12-19 13:26:26 -0500

    added functionality to display data in percentage when single filtering, added ability to extract sample size and display on graph

--- sauti/src/Components/Graph.js
+++ sauti/src/Components/Graph.js
@@ -2,43 +2,18 @@ import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
 import "./temp.css";
 
-<<<<<<< HEAD
-const firstLetterUpper = word => {
-  let split = word.split("");
-  split[0].toUpperCase();
-  let joined = split.join("");
-  return joined;
-};
-
-const getMaxValue = data => {
-  let max = 0;
-  data.forEach(obj => {
-    let values = Object.values(obj);
-    values = values
-      .map(i => parseInt(i, 10))
-      .filter(item => Number(item) === item);
-    let possMax = Math.max(...values);
-    if (possMax > max) {
-      max = possMax;
-    }
-  });
-  return max;
-};
-
 const Graph = props => {
-  console.log(props.keys);
-  console.log(props.data);
   return (
     <div className="Graph-Container">
       <ResponsiveBar
         data={props.data}
         keys={props.keys}
         indexBy={props.indexBy}
-        groupMode={"grouped"} // Possibly add toggle selector to change group mode.
-        margin={{ top: 60, right: 130, bottom: 75, left: 80 }}
+        groupMode={props.groupMode} // Possibly add toggle selector to change group mode.
+        margin={{ top: 50, right: 170, bottom: 75, left: 80 }}
         padding={0.3}
         innerPadding={0}
-        maxValue={getMaxValue(props.data)}
+        maxValue={100}
         colors={{ scheme: "nivo" }}
         borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
         axisTop={null}
@@ -47,9 +22,12 @@ const Graph = props => {
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: firstLetterUpper(props.indexBy),
+          legend:
+            props.label +
+            " (values as percent of total)," +
+            ` sample size = ${props.sampleSize}`,
           legendPosition: "middle",
-          legendOffset: 32
+          legendOffset: 35
         }}
         axisLeft={{
           tickSize: 5,
@@ -57,11 +35,12 @@ const Graph = props => {
           tickRotation: 0,
           legend: "Quantity", // Possibly toggle percentage or number in future release
           legendPosition: "middle",
-          legendOffset: -40
+          legendOffset: -60
         }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
+        labelSkipWidth={0}
+        labelSkipHeight={0}
         labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+        // labelFormat={"%"}
         legends={[
           {
             dataFrom: "keys",
@@ -92,87 +71,6 @@ const Graph = props => {
       />
     </div>
   );
-=======
-const getMaxValue = data => {
-    let max = 0
-    data.forEach(obj => {
-        let values = Object.values(obj)
-        values = values.map(i => parseInt(i, 10)).filter(item => Number(item) === item)
-        let possMax = Math.max(...values)
-        if(possMax > max){
-            max = possMax
-        }
-    })
-    
-    return max * 1.1
-}
-
-const Graph = props => {
-        return (
-           <div className="Graph-Container">
-              <ResponsiveBar
-                data={props.data}
-                keys={props.keys}
-                indexBy={props.indexBy}
-                groupMode={props.groupMode} // Possibly add toggle selector to change group mode.
-                margin={{ top: 50, right: 170, bottom: 75, left: 80 }}
-                padding={0.3}
-                innerPadding={0}
-                maxValue={getMaxValue(props.data)}
-                colors={{ scheme: 'nivo' }}
-                borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: props.label,
-                    legendPosition: 'middle',
-                    legendOffset: 35
-                }}
-                axisLeft={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'Quantity', // Possibly toggle percentage or number in future release
-                    legendPosition: 'middle',
-                    legendOffset: -60
-                }}
-                labelSkipWidth={0}
-                labelSkipHeight={0}
-                labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-                legends={[
-                    {
-                        dataFrom: 'keys',
-                        anchor: 'bottom-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 120,
-                        translateY: 0,
-                        itemsSpacing: 2,
-                        itemWidth: 100,
-                        itemHeight: 20,
-                        itemDirection: 'left-to-right',
-                        itemOpacity: 0.85,
-                        symbolSize: 20,
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemOpacity: 1
-                                }
-                            }
-                        ]
-                    }
-                ]}
-                animate={true}
-                motionStiffness={90}
-                motionDamping={15}
-            />
-           </div>
-        )
->>>>>>> 4fab741e3e5685d3c77ee36fabff94b160c19d58
 };
 
 export default Graph;
--- sauti/src/Components/dataParse.js
+++ sauti/src/Components/dataParse.js
@@ -1,80 +1,91 @@
-import graphLabels from './graphLabels'
+import graphLabels from "./graphLabels";
 
 const dataParse = (indexBy, data, crossFilter) => {
-    const dataStructure = graphLabels[`${indexBy}`].structure
+  const dataStructure = graphLabels[`${indexBy}`].structure;
 
-    // setItem(data, keys, "education", "gender")
-    if(crossFilter !== ""){
-    return setCrossedItems(data, dataStructure, crossFilter, indexBy)
-    } else {
-    return setItem(data, dataStructure, indexBy)
-    }
-}
-
+  // setItem(data, keys, "education", "gender")
+  if (crossFilter !== "") {
+    return setCrossedItems(data, dataStructure, crossFilter, indexBy);
+  } else {
+    return setItem(data, dataStructure, indexBy);
+  }
+};
 
 // THESE NEED TO BE IN CORRECT ORDER OR FUNCTION WILL NOT WORK
 // NEED TO FIND A WAY TO GET THEM INTO A SPECIFIC ORDER
 const getIndex = (data, indexBy) => {
-    // Shrinks objects to one single key:value pair specified by the indexBy
-    const cleanedArr = data.map(item => item = { [`${indexBy}`]: item[`${indexBy}`] })
+  // Shrinks objects to one single key:value pair specified by the indexBy
+  const cleanedArr = data.map(
+    item => (item = { [`${indexBy}`]: item[`${indexBy}`] })
+  );
 
-    // Reduces down to a set of the possible key:value pairs
-    const reducedArr = [...new Set(cleanedArr.map(JSON.stringify))].map(JSON.parse)
+  // Reduces down to a set of the possible key:value pairs
+  const reducedArr = [...new Set(cleanedArr.map(JSON.stringify))].map(
+    JSON.parse
+  );
 
-    return reducedArr;
-    // [{gender: male}, {gender: female}, {gender: null},]
-}
+  return reducedArr;
+  // [{gender: male}, {gender: female}, {gender: null},]
+};
 
 const setCrossedItems = (data, dataStructure, crossFilter, indexBy) => {
-    const keysArr = [];
-    let crossFilterKeysArr = [];
+  const keysArr = [];
+  let crossFilterKeysArr = [];
 
-    const crossFilterKeys = graphLabels[`${crossFilter}`].structure;
+  const crossFilterKeys = graphLabels[`${crossFilter}`].structure;
 
+  // Puts each value from key:value pair into an array
+  // ['Female', 'Male', null]
+  dataStructure.forEach(obj => keysArr.push(Object.values(obj)[0]));
+  crossFilterKeys.forEach(
+    obj =>
+      Object.values(obj)[0] !== null &&
+      crossFilterKeysArr.push(Object.values(obj)[0])
+  );
 
-    // Puts each value from key:value pair into an array
-    // ['Female', 'Male', null]
-    dataStructure.forEach(obj => keysArr.push(Object.values(obj)[0]));
-    crossFilterKeys.forEach(obj => Object.values(obj)[0] !== null && crossFilterKeysArr.push(Object.values(obj)[0]));
+  // For each object in the array,
+  keysArr.forEach((key, index) => {
+    // Gets every trader at the index where it equals the value in the keysArr
+    const filtered = data.filter(trader => trader[`${indexBy}`] === key);
 
-    // For each object in the array, 
-    keysArr.forEach((key, index) => {
-        // Gets every trader at the index where it equals the value in the keysArr
-        const filtered = data.filter(trader => trader[`${indexBy}`] === key)
-        
-        // Gets every trader at the crossFilter where it equals the value in the crossFilterKeysArr
-        // Then pushes into crossFilteredData
-        const crossFilteredData = [];
-        crossFilterKeysArr.forEach((key, index) => {
-            const crossFiltered = filtered.filter(trader => trader[`${crossFilter}`] === key)
-            crossFilteredData.push({ [`${key}`]: crossFiltered.length })
-        })
+    // Gets every trader at the crossFilter where it equals the value in the crossFilterKeysArr
+    // Then pushes into crossFilteredData
+    const crossFilteredData = [];
+    crossFilterKeysArr.forEach((key, index) => {
+      const crossFiltered = filtered.filter(
+        trader => trader[`${crossFilter}`] === key
+      );
+      crossFilteredData.push({ [`${key}`]: crossFiltered.length });
+    });
 
-        // Builds the object that will be sent to the graph component
-        crossFilteredData.forEach(obj => {
-                return dataStructure[index] = {...dataStructure[index], [`${Object.keys(obj)[0]}`]: [`${Object.values(obj)[0]}`][0] } 
-        })
-    })
+    // Builds the object that will be sent to the graph component
+    crossFilteredData.forEach(obj => {
+      return (dataStructure[index] = {
+        ...dataStructure[index],
+        [`${Object.keys(obj)[0]}`]: [`${Object.values(obj)[0]}`][0]
+      });
+    });
+  });
 
-    // Replaces "null" values with "No Response"
-    // keys.forEach(obj => obj[`${indexBy}`] === null && (obj[`${indexBy}`] = "No Response"));
-    // crossFilterKeysArr = crossFilterKeysArr.map(key => key === null ? "No Response" : key);
-    // keys.forEach(obj => {
-    //     obj[`${indexBy}`] === null && (obj[`${indexBy}`] = "No Response")   ////////////////////////NEED TO REMOVE NULL RESPONSES
-    //     if(obj[null]){
-    //     obj["No Response"] = obj[null]
-    //     delete obj[null] 
-    //     }
-    // });
+  // Replaces "null" values with "No Response"
+  // keys.forEach(obj => obj[`${indexBy}`] === null && (obj[`${indexBy}`] = "No Response"));
+  // crossFilterKeysArr = crossFilterKeysArr.map(key => key === null ? "No Response" : key);
+  // keys.forEach(obj => {
+  //     obj[`${indexBy}`] === null && (obj[`${indexBy}`] = "No Response")   ////////////////////////NEED TO REMOVE NULL RESPONSES
+  //     if(obj[null]){
+  //     obj["No Response"] = obj[null]
+  //     delete obj[null]
+  //     }
+  // });
 
-    console.log('dataStructure', dataStructure)
-    console.log('crossfilter', crossFilterKeysArr)
+  console.log("dataStructure", dataStructure);
+  console.log("crossfilter", crossFilterKeysArr);
 
-    return { dataStructure, crossFilterKeysArr, indexBy };
+  return { dataStructure, crossFilterKeysArr, indexBy };
 };
 
 // const crossFilterWithoutNulls = (keys, crossFilterKeysArr, indexBy) => {
-    
+
 //     crossFilterKeysArr = crossFilterKeysArr.filter(item => item !== "No Response")
 
 //     keys.forEach((obj, index) => {
@@ -83,35 +94,52 @@ const setCrossedItems = (data, dataStructure, crossFilter, indexBy) => {
 // }
 
 const setItem = (data, dataStructure, indexBy) => {
-    let arr = [];
+  let arr = [];
 
-    // Puts each value from key:value pair into an array
-    // ['Female', 'Male', null]
-    dataStructure.forEach(obj => arr.push(Object.values(obj)[0]))
+  // Puts each value from key:value pair into an array
+  // ['Female', 'Male', null]
+  dataStructure.forEach(obj => arr.push(Object.values(obj)[0]));
 
-    // For each object in the array, 
-    arr.forEach((key, index) => {
-        // Gets every trader at the index where it equals the value in the arr
-       const filtered = data.filter(trader => trader[`${indexBy}`] === key).length
+  // For each object in the array,
+  arr.forEach((key, index) => {
+    // Gets every trader at the index where it equals the value in the arr
+    const filtered = data.filter(trader => trader[`${indexBy}`] === key).length;
 
-       dataStructure[index] = {
-            ...dataStructure[index],
-            [`${arr[index]}`]: filtered
-        }
-    })
+    dataStructure[index] = {
+      ...dataStructure[index],
+      [`${arr[index]}`]: filtered
+    };
+  });
 
-    // keys.forEach(obj => {
-    //     obj[`${indexBy}`] === null && (obj[`${indexBy}`] = "No Response")
-    //     if(obj[null]){
-    //     obj["No Response"] = obj[null]
-    //     delete obj[null] 
-    //     }
-    // });
-    
+  // keys.forEach(obj => {
+  //     obj[`${indexBy}`] === null && (obj[`${indexBy}`] = "No Response")
+  //     if(obj[null]){
+  //     obj["No Response"] = obj[null]
+  //     delete obj[null]
+  //     }
+  // });
 
-    // arr = arr.map(item => item === null ? "No Response" : item);
-    
-    return { dataStructure, arr, indexBy} ;
-}
+  // arr = arr.map(item => item === null ? "No Response" : item);
+  console.log("this is datastructure", dataStructure);
 
-export default dataParse
+  let numberValues = [];
+
+  let sampleSize = 0;
+
+  dataStructure.map(item => {
+    const keyValue = item[`${indexBy}`];
+    numberValues.push(Number(item[keyValue]));
+    sampleSize += Number(item[keyValue]);
+  });
+  console.log("sampleSize", sampleSize);
+  console.log("this", numberValues);
+
+  dataStructure.forEach(obj => {
+    const keyValue = obj[`${indexBy}`];
+    obj[keyValue] = Math.round((obj[keyValue] / sampleSize) * 100);
+  });
+  console.log("final dataStructure", dataStructure);
+  return { dataStructure, arr, indexBy, sampleSize };
+};
+
+export default dataParse;
