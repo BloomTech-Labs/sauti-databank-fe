a28e2c9500f63261521329bf5dfddac283b943b4
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-20 15:44:12 -0800

    One app test

--- sauti/src/App.test.js
+++ sauti/src/App.test.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
-import { MockedProvider, MockedResponse } from '@apollo/react-testing';
+import { MockedProvider} from '@apollo/react-testing';
 import {render, cleanup} from '@testing-library/react';
+import Queries from './Components/Queries';
 
-// const mocks = [
-//   {
-//     request: {
-//       query: SOME_QUERY,
-//       variables: { first: 4 }
-//     },
-//     result: {
-//       data: {
-//         dog: {
-//           name: "The Rock"
-//         }
-//       }
-//     }
-//   },
-//   {
-//     request: {
-//       query: SOME_QUERY,
-//       variables: { first: 8}
-//     },
-//     error: new Error("Something went wrong")
-//   }
-// ]
 
 afterEach(cleanup);
 
-test('Renders without crashing:', async () => {
+test('App Renders Without Crashing:', async () => {
   const {debug} = render(
-    <MockedProvider >
+    <MockedProvider  >
       <Router>
         <App />
       </Router>
