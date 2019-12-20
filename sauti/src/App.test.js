635031a6c4986a5d4ebdff8327f99646280dffa8
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-20 13:01:56 -0800

    added jest and started app.test.js

--- sauti/src/App.test.js
+++ sauti/src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { MockedProvider } from '@apollo/react-testing';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+// it('DOES APP RENDER, DAMMIT?!', () => {
+//     ReactDOM.render(
+//         <App />,
+//   document.getElementById("root")
+
+//     );
+//   });
+
+// describe('App', function(){
+//   describe('Rendering of Things in App.js', function(){
+
+    test('DOES APP RENDER?:', () => {
+        ReactDOM.render(
+          <App />
+        );
+    });
+
+//     let container = null;
+//     beforeEach(()=> {
+//       //setup DOM element as render target
+//       container = document.createElement('div');
+//       document.body.appendChild(container);
+//     });
+
+//     afterEach(()=> {
+//       //cleanup on exit
+//       unmountComponentAtNode(container);
+//       container.remove();
+//       container = null;
+//     });
+
+//   })
+// })
+
