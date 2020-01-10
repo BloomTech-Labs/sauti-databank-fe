68f4bbb3628bfe813d9ec5bebc4f5745bf570971
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-01-09 16:37:43 -0800

    cleaned up filterbox, resized date container

--- sauti/src/Components/FilterBox.js
+++ sauti/src/Components/FilterBox.js
@@ -5,102 +5,6 @@ import styled from 'styled-components';
 import Dropdown from 'react-dropdown';
 import {FilterBoxOptions} from './FilterBoxOptions';
 
-const FilterOption = styled.p`
-  margin-left: .5rem;
-  margin-top: .5rem;
-  font-size: 1rem;
-`
-
-const Options = styled.div`
-  display: flex;
-  align-items: center;
-  input [type='radio']{
-    background-color: red;
-    font-size: 16px;
-    background-image: none;
-  }
-`
-const OptionContainer = styled.div`
-  max-height: 40vh;
-  overflow-x: hidden;
-  overflow-y: auto;
-  margin: 10px 0;
-  padding-bottom: 10px;
-  border-bottom: 1px solid #ccc;
-`;
-
-const DateContainer = styled.div`
-  margin: 20px 0;
-  display: flex;
-  justify-content: space-between;
-  div{
-    display: flex;
-    flex-direction: column;
-    input{
-      font-family: 'Helvetica', sans-serif;  
-      font-size: 16px;
-      margin: 0; 
-      border-radius: 2px;
-      border: 1px solid #ccc;
-      padding: 8px 0 8px 4px;
-      ::-webkit-inner-spin-button {display: none};
-      ::-webkit-clear-button{display: none};
-      ::-webkit-calendar-picker-indicator{ 
-        background: url(https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/calendar-16.png) no-repeat; 
-        opacity: 0.8; 
-        cursor: pointer
-      };
-    };
-  }
-`;
-
-const Button = styled.div`
-  background: #47837F;
-  max-width: 40%;
-  color: #fff;
-  font-weight: 400;
-  padding: 10px;
-  margin: auto;
-  text-align: center;
-  align-self: center;
-  font-size: 1.5rem;
-  :hover{cursor: pointer};
-`;
-
-const DropdownContainer = styled.div`
-font-family: Helvetica, sans-serif;
-        color: $greyColor;
-        font-weight: bold;
-
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        width: 26.9rem;
-
-        p {
-          font-size: 1.3rem;
-          margin-bottom: 10px;
-        }
-
-        .dropdown {
-          color: $greyColor;
-          font-size: 1.6rem;
-          font-weight: normal;
-          display: flex;
-          align-items: center;
-        }
-        .myControlClassName {
-          width: 100%;
-
-          padding-top: 15px;
-          padding-bottom: 15px;
-          display: flex;
-          align-items: center;
-        }
-        .Dropdown-arrow {
-          position: absolute;
-          top: 21px;
-          right: 15px;`;
 
 export default function FilterBox(props) {
   const [options, setOptions] = useState(FilterBoxOptions.default);
@@ -205,7 +109,7 @@ export default function FilterBox(props) {
           
           )}
 
-          <p onClick={e=> {
+          <p className='reset-btn' onClick={e=> {
             props.setCrossLabel('')
             props.setCrossFilter({type: '', query: 'Users'})
           }}>Reset</p>
@@ -214,3 +118,106 @@ export default function FilterBox(props) {
     </DropdownContainer>
   )
 }
+
+
+const FilterOption = styled.p`
+  margin-left: .5rem;
+  margin-top: .5rem;
+  font-size: 1rem;
+`
+
+const Options = styled.div`
+  display: flex;
+  align-items: center;
+  input [type='radio']{
+    background-color: red;
+    font-size: 16px;
+    background-image: none;
+  }
+`
+const OptionContainer = styled.div`
+  max-height: 40vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  margin: 10px 0;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ccc;
+`;
+
+const DateContainer = styled.div`
+  margin: 20px 0;
+  display: flex;  
+  div{
+    display: flex;
+    flex-direction: column;
+    max-width: 50%;
+    input{
+      font-family: 'Helvetica', sans-serif;  
+      font-size: 16px;
+      margin: 0; 
+      border-radius: 2px;
+      border: 1px solid #ccc;
+      padding: 8px 0 8px 4px;
+      ::-webkit-inner-spin-button {display: none};
+      ::-webkit-clear-button{display: none};
+      ::-webkit-calendar-picker-indicator{ 
+        background: url(https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/calendar-16.png) no-repeat; 
+        opacity: 0.8; 
+        cursor: pointer
+      };
+    };
+  }
+`;
+
+const Button = styled.div`
+  background: #47837F;
+  max-width: 40%;
+  color: #fff;
+  font-weight: 400;
+  padding: 10px;
+  margin: auto;
+  text-align: center;
+  align-self: center;
+  font-size: 1.5rem;
+  :hover{cursor: pointer};
+`;
+
+const DropdownContainer = styled.div`
+font-family: Helvetica, sans-serif;
+        color: $greyColor;
+        font-weight: bold;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        width: 26.9rem;
+
+        p {
+          font-size: 1.3rem;
+          margin-bottom: 10px;
+          
+        }
+        .reset-btn{
+            text-decoration: underline;
+            opacity: .7;
+            cursor: pointer
+        }
+        .dropdown {
+          color: $greyColor;
+          font-size: 1.6rem;
+          font-weight: normal;
+          display: flex;
+          align-items: center;
+        }
+        .myControlClassName {
+          width: 100%;
+
+          padding-top: 15px;
+          padding-bottom: 15px;
+          display: flex;
+          align-items: center;
+        }
+        .Dropdown-arrow {
+          position: absolute;
+          top: 21px;
+          right: 15px;`;
