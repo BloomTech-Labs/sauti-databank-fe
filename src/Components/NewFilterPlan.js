552e52102554e365334d5bacad81e4fcb42ce107
David Tauraso <david318@me.com>
2020-03-04 13:50:55 -0800

    Updated in depth filtering options

--- src/Components/NewFilterPlan.js
+++ src/Components/NewFilterPlan.js
@@ -67,90 +67,238 @@ var people = [
 ];
 // each round take the first one out and only show the remaining ones (done)
 const Node = props => {
-  console.log("was called", props);
-  const [isSelected, setIsSelected] = useState(false);
-  const [subcategories, setSubcategories] = useState([]);
-  const [remainingCategories, setRemainingCategories] = useState([]);
+  console.log("props", props);
+  //   const [prompt, setPrompt] = useState(props.)
+  // const [isSelected, setIsSelected] = useState(false);
+  const { prompt, ...rest } = props.currentCategory;
+  const { subcategories, ...rest2 } = props;
+  const [subcategoriesToDisplay, setSubcategoriesToDisplay] = useState([]);
+  const [
+    remainingCategoriesToDisplay,
+    setRemainingCategoriesToDisplay
+  ] = useState([]);
+  const remainingCategories = props.categories2.filter(
+    category => category.prompt !== prompt
+  );
+  console.log("prompt", prompt);
+  console.log("choices", subcategories);
+  console.log("remaining categories", remainingCategories);
+  console.log("\n");
+  // currentCategory={props.categories2[i]} remainingCategories[i]
+  // subcategories={props.categories2[i].subcategories} remainingCategories[i].subcategories
+
+  //   const [prompt, setPrompt] = useState(props.currentCategory.prompt)
+  //   const [subcategories, setSubcategories] = useState(props.subcategories);
+
+  //   const [remainingCategories, setRemainingCategories] = useState([]);
   // const count = 0
   let childnodes = null;
   const [childNodes2, setChildNodes2] = useState([]);
   // the Node component calls itself if there are children
-  const f = () => {
-    // console.log(props.categories2)
-    if (props.categories2.length > 0) {
-      // if(isSelected) {
-      console.log("got here");
-      const [firstOne, ...rest] = props.categories;
-      // console.log(firstOne)
-      console.log(props.categories2);
-      const [firstOne2, ...rest2] = props.categories2;
-      console.log("first one 2", props.firstOne2);
-      // setSubcategories()
-      // can't use rerender triggers unless its directly coming from a button
-      setSubcategories(props.firstOne2.subcategories);
-      console.log(subcategories);
-      // console.log("children", props.children)
-      // childnodes = props.categories2.map((childnode, i) => {
-      setChildNodes2(
-        props.categories2.map((childnode, i) => {
-          // console.log(count + i)
-
+  const x = () => {
+    // setIsSelected(true);
+    if (subcategories.length > 0) {
+      //   console.log("we clicked")
+      //   console.log(props.categories2)
+      //   console.log(props.categories2[0].subcategories)
+      console.log("next ones", subcategories, remainingCategories);
+      setSubcategoriesToDisplay(
+        subcategories.map((subcategory, i) => {
+          return <li key={i}>{subcategory.prompt + " OR"}</li>;
+        })
+      );
+      // subcategories.map((subcategory, i) => {
+      //     return <li key={i}>{subcategory.prompt + " OR"}</li>;
+      //   })
+      // subcategories.length != remainingCategories.length
+      //   setSubcategoriesToDisplay(subcategories.map((subcategory, i) => {
+      //     return (
+      //     <Node
+      //             key={i}
+      //             currentCategory={remainingCategories[i]}
+      //             subcategories={remainingCategories[i].subcategories}
+      //             // firstOne2={firstCategory}
+      //             // rest3={rest2}
+      //             // prompt2={firstCategory.prompt}
+      //             // subcategories={firstCategory.subcategories}
+      //             // categories={rest}
+      //             // setCategories={setCategories}
+      //             // firstOne={firstOne}
+      //             categories2={remainingCategories}
+      //             // setCategories2={setSubcategoriesToDisplay}
+      //             // count={count + i}
+      //         />
+      //         )}))
+    }
+    if (remainingCategories.length > 0) {
+      setRemainingCategoriesToDisplay(
+        remainingCategories.map((subcategory, i) => {
           return (
             <Node
               key={i}
-              categories={rest}
-              setCategories={props.setCategories}
-              firstOne={firstOne}
-              categories2={rest2}
-              setCategories2={props.setCategories2}
-              firstOne2={firstOne2}
-              // count={props.count + i}
+              currentCategory={remainingCategories[i]}
+              subcategories={remainingCategories[i].subcategories}
+              // firstOne2={firstCategory}
+              // rest3={rest2}
+              // prompt2={firstCategory.prompt}
+              // subcategories={firstCategory.subcategories}
+              // categories={rest}
+              // setCategories={setCategories}
+              // firstOne={firstOne}
+              categories2={remainingCategories}
+              // setCategories2={setSubcategoriesToDisplay}
+              // count={count + i}
             />
           );
         })
       );
-      // }
     }
+  };
+  const f = () => {
+    // if we have subcategories to fill
+    // if(subcategoriesToDisplay.length > 0) {
+    //     subcategoriesToDisplay.map(
+    //     )
+    // <Node
+    //             key={i}
+    //             currentCategory={props.categories2[i]}
+    //             subcategories={props.categories2[i].subcategories}
+    //             // firstOne2={firstCategory}
+    //             // rest3={rest2}
+    //             // prompt2={firstCategory.prompt}
+    //             // subcategories={firstCategory.subcategories}
+    //             // categories={rest}
+    //             // setCategories={setCategories}
+    //             // firstOne={firstOne}
+    //             // categories2={categories2}
+    //             // setCategories2={setCategories2}
+    //             // count={count + i}
+    //         />
+    // }
+    // if we have remaining categories to fill
+    // if(remainingCategories.length > 0) {
+    // }
+    // sort of holding both containers (suboptions, and the catagroies)
+    // hostage unless there is another level to go down
+    // console.log("f was called", props.categories2)
+    // if (props.categories2.length > 0) {
+    // if(isSelected) {
+    //   console.log("got here");
+    //   const [firstOne, ...rest] = props.categories;
+    // console.log(firstOne)
+    //   console.log(props.categories2);
+    //   const [firstOne2, ...rest2] = props.categories2;
+    //   console.log("first one 2", props.firstOne2);
+    // setSubcategories()
+    // can't use rerender triggers unless its directly coming from a button
+    //   setSubcategories(props.firstOne2.subcategories);
+    //   console.log(subcategories);
+    // console.log("children", props.children)
+    // childnodes = props.categories2.map((childnode, i) => {
+    //   setChildNodes2(
+    //     props.categories2.map((childnode, i) => {
+    //       // console.log(count + i)
+    //       return (
+    //         <Node
+    //           key={i}
+    //         //   categories={rest}
+    //         //   setCategories={props.setCategories}
+    //         //   firstOne={firstOne}
+    //           categories2={rest2}
+    //           setCategories2={props.setCategories2}
+    //           firstOne2={firstOne2}
+    //           // count={props.count + i}
+    //         />
+    //       );
+    //     })
+    //   );
+    // }
+    // }
   };
 
   // return our list element
   // display children if there are any
-  console.log("sub", subcategories);
-
+  //   console.log("sub", subcategories);
+  // return (
+  //     <div></div>
+  // )
   return (
-    // <li key={props.count}>
-    <li>
-      {/* on prompt click display the suboptions for the category and the remaining categories they can choose */}
-
-      <span
+    <div>
+      {/* // <li key={props.count}> */}
+      <li>
+        {/* on prompt click display the suboptions for the category and the remaining categories they can choose */}
+        {/* smallest case */}
+        <span
+          onClick={() => {
+            x();
+          }}
+        >
+          {/* the moment this line runs(it appeared to be trying to use something that couldn't have been set yet
+            and that's why it fired another render call) it does another call */}
+          {JSON.stringify(props.currentCategory.prompt) + " AND"}
+        </span>
+        {/* <span
         onClick={() => {
           console.log("i was clicked");
-          setIsSelected(true);
+        //   setIsSelected(true);
 
-          f();
+        //   f();
         }}
       >
-        {props.firstOne2.prompt}
-      </span>
-      {/* subcategories are male, female */}
-      {subcategories.length > 0 ? (
+        {props.firstOne2.prompt + " AND"}
+      </span> */}
+        {/* only load the next set if we have selected and the length > 0 */}
+        {/* {x()} */}
+        <ul>{subcategoriesToDisplay}</ul>
+        <ul>{remainingCategoriesToDisplay}</ul>
+
+        {/* {subcategoriesToDisplay.length > 0?
+      // infinite call stack
+        subcategoriesToDisplay.map((subcategory, i) => {
+            // return (
+            // <Node
+            //         key={i}
+            //         currentCategory={props.categories2[i]}
+            //         subcategories={props.categories2[i].subcategories}
+            //         // firstOne2={firstCategory}
+            //         // rest3={rest2}
+            //         // prompt2={firstCategory.prompt}
+            //         // subcategories={firstCategory.subcategories}
+            //         // categories={rest}
+            //         // setCategories={setCategories}
+            //         // firstOne={firstOne}
+            //         categories2={props.categories2}
+            //         setCategories2={props.setCategories2}
+            //         // count={count + i}
+            //     />
+                // )
+        })
+        :
+            []
+      }} */}
+        {/* subproblem case1 */}
+        {/* subcategories are male, female */}
+        {/* {subcategories.length > 0 ? (
         // only want to display this if the user clicks the prompt
         <ul>
           {subcategories.map((subcategory, i) => {
-            return <li key={i}>{subcategory.prompt}</li>;
+            return <li key={i}>{subcategory.prompt + " OR"}</li>;
           })}
         </ul>
       ) : (
         []
-      )}
+      )} */}
 
-      {/* {   childnodes ?
+        {/* subproblem case1 */}
+
+        {/* {   childnodes ?
             <ul>{childnodes}</ul>:
             null } */}
-      {/* {console.log(childNodes2)} */}
-      {/* childNodes2 are components with prompt as country, income, education */}
-      {childNodes2 ? <ul>{childNodes2}</ul> : []}
-    </li>
+        {/* {console.log(childNodes2)} */}
+        {/* childNodes2 are components with prompt as country, income, education */}
+        {/* {childNodes2 ? <ul>{childNodes2}</ul> : []} */}
+      </li>
+    </div>
   );
 };
 
@@ -160,29 +308,66 @@ export const Organisation = props => {
   const [categories2, setCategories2] = useState(props.categories2);
 
   const count = 0;
-  const [firstOne, ...rest] = categories;
-  const [firstOne2, ...rest2] = categories2;
+  //   const [firstOne, ...rest] = categories;
+  //   const [firstCategory, ...rest2] = categories2;
   // console.log(firstOne)
-  console.log(firstOne2);
+  console.log("categories2", categories2);
 
-  let nodes = categories2.map((person, i) => {
-    return (
-      <Node
-        key={i}
-        categories={rest}
-        setCategories={setCategories}
-        firstOne={firstOne}
-        categories2={rest2}
-        setCategories2={setCategories2}
-        firstOne2={firstOne2}
-        // count={count + i}
-      />
-    );
-  });
-
+  // not start of recusion pipeline
+  /*
+    
+    should run [f(cat1), f(cat2), f(cat3)]
+    */
+  //   let nodes = categories2.map((category, i) => {
+  //     return (
+  //       <Node
+  //         key={i}
+  //         currentCategory={category}
+  //         // prompt2={firstCategory.prompt}
+  //         // subcategories={firstCategory.subcategories}
+  //         // categories={rest}
+  //         // setCategories={setCategories}
+  //         // firstOne={firstOne}
+  //         categories2={categories2}
+  //         setCategories2={setCategories2}
+  //         // firstOne2={firstCategory}
+  //         // count={count + i}
+  //       />
+  //     );
+  //   });
+  /*
+category0
+    subs
+    category1
+*/
   return (
     <div>
-      <ul className="org">{nodes}</ul>
+      <ul className="org">
+        {/* {nodes} */}
+        {/* when each of these gets fired off, the lower options need to already be ready
+          and the next categories need to already be ready */}
+        {categories2.map((category, i) => {
+          // const firstCategory = props.categories2[i];
+
+          return (
+            <Node
+              key={i}
+              currentCategory={props.categories2[i]}
+              subcategories={props.categories2[i].subcategories}
+              // firstOne2={firstCategory}
+              // rest3={rest2}
+              // prompt2={firstCategory.prompt}
+              // subcategories={firstCategory.subcategories}
+              // categories={rest}
+              // setCategories={setCategories}
+              // firstOne={firstOne}
+              categories2={categories2}
+              setCategories2={setCategories2}
+              // count={count + i}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 };
