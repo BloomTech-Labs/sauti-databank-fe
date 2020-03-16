f3a1c9da8ca0aadf899aada3416f1cf3e7a6415b
vpagano10 <vpagano10@gmail.com>
2020-03-16 12:32:00 -0400

    Added a hide/show button on the data page that hides the filter options and extends the graph.

--- src/GraphContainer.js
+++ src/GraphContainer.js
@@ -9,6 +9,8 @@ import { withRouter } from "react-router-dom";
 import Queries from "./Components/Queries2";
 import useCalendar from "../src/hooks/useCalendar";
 
+import styled from "styled-components";
+
 const GraphContainer = () => {
   const [index, setIndex] = useState({
     type: "gender",
@@ -62,14 +64,23 @@ const GraphContainer = () => {
     setCrossFilter(e.target.value);
   };
 
+  const [hidden, setHidden] = useState(false);
+
+  function HideFilters() {
+    setHidden(!hidden);
+  }
+
   return (
     <div className="App">
       <div className="main-container">
-        <div className="header">
+        <TopBar className="header">
           <h1>Informal Cross-Border Trade Data</h1>
-        </div>
+          <FilterHideButton onClick={HideFilters}>
+            {hidden ? <p>Show Filters</p> : <p>Hide Filters</p>}
+          </FilterHideButton>
+        </TopBar>
         <div className="content-container">
-          <div className="chart-container">
+          <div className={hidden ? "extend" : "chart-container"}>
             <Queries
               index={index}
               crossFilter={crossFilter}
@@ -90,7 +101,11 @@ const GraphContainer = () => {
               setFilterBoxEndDate={setFilterBoxEndDate}
             />
           </div>
-          <div className="dropdown-container">
+          <div
+            className={
+              hidden ? "dropdown-container hide" : "dropdown-container"
+            }
+          >
             <FilterBox
               onChange={onChange}
               onSubmit={onSubmit}
@@ -124,3 +139,23 @@ const GraphContainer = () => {
 };
 
 export default withRouter(GraphContainer);
+
+const FilterHideButton = styled.button`
+  padding: 20px;
+  width: 200px;
+  border: 2px solid #eb5e52;
+  border-radius: 5px;
+  background-color: #eb5e52;
+  color: white;
+  margin-right: 20px;
+  transition: 0.5s ease;
+  &:hover {
+    background-color: white;
+    color: black;
+    cursor: pointer;
+  }
+`;
+const TopBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
