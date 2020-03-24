1f11707b56ccb28548bc8761b2bdfcc111c6abe4
vpagano10 <vpagano10@gmail.com>
2020-03-24 13:40:53 -0400

    Updated graph home page for links and copy url.
    Co-authored-by: Joshua Hill <Josh_Hill15@me.com>

--- src/GraphContainer.js
+++ src/GraphContainer.js
@@ -9,6 +9,8 @@ import { withRouter } from "react-router-dom";
 import Queries from "./Components/Queries2";
 import useCalendar from "../src/hooks/useCalendar";
 import styled from "styled-components";
+import swal from "sweetalert";
+import ClipboardJS from "clipboard";
 
 const GraphContainer = () => {
   const [filters, setFilters] = useState({
@@ -62,45 +64,60 @@ const GraphContainer = () => {
     setFilterBoxEndDate
   } = useCalendar();
 
-  /*
-breaks search(creates an undefined search)
-{0: {…}, 1: {…}, 2: {…}}
-0:
-selectedTableColumnName: "crossing_freq"
-selectedTable: "Users"
-selectedCategory: "Border Crossing Frequency"
-selectedOption: undefined
-__proto__: Object
-1:
-selectedCategory: ""
-selectedOption: undefined
-selectedTable: ""
-selectedTableColumnName: ""
-__proto__: Object
-2:
-selectedCategory: "Most Requested Procedure Commodity Categories"
-selectedOption: undefined
-selectedTable: "Sessions"
-selectedTableColumnName: "procedurecommoditycat"
-*/
-
   const [hidden, setHidden] = useState(false);
 
   function HideFilters() {
     setHidden(!hidden);
   }
 
+  const clipboard = new ClipboardJS(".btn", {
+    text: function() {
+      return document.location.href;
+    }
+  });
+  clipboard.on("success", function(e) {
+    swal({ title: "", text: "copied url!", icon: "success" });
+  });
+
   return (
     <div className="App">
       <div className="main-container">
         <div className="header">
           <h1>Informal Cross-Border Trade Data</h1>
-          {/* <FilterHideButton onClick={HideFilters}>
-            {hidden ? <p>Show Filters</p> : <p>Hide Filters</p>}
-          </FilterHideButton> */}
         </div>
         <div className="content-container">
-          <div className={hidden ? "extend" : "chart-container"}>
+          <ContentContainerDiv
+            className={hidden ? "extend" : "chart-container"}
+          >
+            <SocialMediaContainer className="social-media-container">
+              <div>
+                <SocialMediaIconsTwitter
+                  class="twitter-share-button"
+                  target="_blank"
+                  href="https://twitter.com/intent/tweet?text=This%20website%20is%20awesome!"
+                >
+                  <i class="fab fa-twitter"> Twitter</i>
+                </SocialMediaIconsTwitter>
+              </div>
+              <div
+                class="fb-share-button"
+                data-href="https://blissful-pare-60612f.netlify.com/data"
+                data-layout="button"
+                data-size="small"
+              >
+                <SocialMediaIconsFacebook
+                  target="_blank"
+                  href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
+                  class="fb-xfbml-parse-ignore"
+                >
+                  <i class="fab fa-facebook-square"> Facebook</i>
+                </SocialMediaIconsFacebook>
+              </div>
+              <CopyUrlButton className="btn">Copy URL</CopyUrlButton>
+              <FilterHideButton onClick={HideFilters}>
+                {hidden ? <p>Show Filters</p> : <p>Hide Filters</p>}
+              </FilterHideButton>
+            </SocialMediaContainer>
             <Queries
               filters={filters}
               filterBoxStartDate={filterBoxStartDate}
@@ -108,11 +125,7 @@ selectedTableColumnName: "procedurecommoditycat"
               filterBoxEndDate={filterBoxEndDate}
               setFilterBoxEndDate={setFilterBoxEndDate}
             />
-          </div>
-          {/* can put a hide/show button here */}
-          <FilterHideButton onClick={HideFilters}>
-            {hidden ? <p>◀</p> : <p>▶</p>}
-          </FilterHideButton>
+          </ContentContainerDiv>
           <div
             className={
               hidden ? "dropdown-container hide" : "dropdown-container"
@@ -136,18 +149,57 @@ selectedTableColumnName: "procedurecommoditycat"
 export default withRouter(GraphContainer);
 
 const FilterHideButton = styled.button`
-  font-size: 2.5rem;
-  font-weight: bold;
-  border: 1px solid grey;
+  padding: 8px 5px;
+  background: #eb5e52;
+  font-weight: 400;
+  color: white;
   border-radius: 5px;
-  background-color: white;
-  color: grey;
-  height: 200px;
-  align-self: center;
-  transition: 0.4s ease;
-  &:hover {
-    background-color: grey;
-    color: white;
+  font-size: 1.4rem;
+  width: 95px;
+  opacity: 0.75;
+  border: none;
+  &: hover {
     cursor: pointer;
+    opacity: 1;
   }
+`;
+const SocialMediaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border-top: 2px solid #47837f;
+  border-bottom: 2px solid #47837f;
+  border-left: 2px solid #47837f;
+  padding: 5px;
+  margin-bottom: 5px;
+`;
+const SocialMediaIconsTwitter = styled.a`
+  font-size: 2rem;
+  margin: 0 10px;
+  color: rgb(0, 172, 238);
+`;
+const SocialMediaIconsFacebook = styled.a`
+  font-size: 2rem;
+  margin: 0 10px;
+  color: rgb(59, 89, 152);
+`;
+
+const CopyUrlButton = styled.button`
+  padding: 8px 5px;
+  background: #47837f;
+  font-weight: 400;
+  color: white;
+  border-radius: 5px;
+  font-size: 1.4rem;
+  width: 95px;
+  opacity: 0.75;
+  border: none;
+  &: hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+`;
+const ContentContainerDiv = styled.div`
+  border-right: 2px solid #47837f;
+  margin-right: 2px;
 `;
