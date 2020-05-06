c76528ef01a741a4f53b00d76a33711f8fd51fe2
Surfsol <surfsol@protonamil.com>
2020-05-06 10:48:31 -0300

    dataParse

--- src/Components/dataParse.js
+++ src/Components/dataParse.js
@@ -16,51 +16,60 @@ const dataParse = (
   queryType,
   crossFilterQuery
 ) => {
-  
-    console.log(
-      "DataParse 7",
-      indexBy,
+  console.log(
+    "DataParse 7",
+    indexBy,
+    data,
+    crossFilter,
+    startDate,
+    endDate,
+    additionalFilter,
+    queryType,
+    crossFilterQuery
+  );
+  let dataStructure = [];
+  //when single filtering "Most Requested" graph
+  console.log(indexBy);
+  //remove multiples should happen prior to filterByDate
+  //multiples should also be given a date
+
+  let newArray = [];
+  if (queryType === "Sessions" && crossFilter === "") {
+    data = data.filter(e => console.log(e.indexBy));
+
+    data = filterByDate(data, startDate, endDate);
+    console.log(`filterByDate`, data);
+    data = removeMultiple(data);
+    console.log(`removemultiple`, data);
+    dataStructure = getIndex(data, indexBy);
+    //console.log(`Data`, data, `dataStructure`,dataStructure, `indexBy`, indexBy)
+    return getMostRequested(data, dataStructure, indexBy);
+  }
+  //when cross-filtering "Most Requested" as index
+  else if (queryType === "Sessions" && crossFilter !== "") {
+    data = filterByDate(data, startDate, endDate);
+
+    data = removeMultiple(data);
+
+    dataStructure = getIndex(data, indexBy);
+
+    return setCrossedItems(
       data,
+      dataStructure,
       crossFilter,
-      startDate,
-      endDate,
+      indexBy,
       additionalFilter,
       queryType,
       crossFilterQuery
     );
-    let dataStructure = [];
-    //when single filtering "Most Requested" graph
-    console.log(indexBy);
-    //remove multiples should happen prior to filterByDate
-    //multiples should also be given a date
-    
-    let newArray = [];
-    if (queryType === "Sessions" && crossFilter === "") {
-      // for (let i = 0; i < data.length; i++) {
-      //   let obj = data[i];
-
-      //   if (obj[indexBy] !== null) {
-      //     newArray.push(obj);
-      //   }
-      // }
-      // console.log(newArray);
-      // data = data.filter(e => console.log(e.indexBy) )
-      // console.log(data)
-     // data = filterByDate(data, startDate, endDate);
-      // console.log(`filterByDate`, data)
-     // data = removeMultiple(data);
-      // console.log(`removemultiple`, data)
-      dataStructure = getIndex(data, indexBy);
-      //console.log(`Data`, data, `dataStructure`,dataStructure, `indexBy`, indexBy)
-      return getMostRequested(data, dataStructure, indexBy);
+  } else {
+    if (queryType === "Users") {
+      dataStructure = graphLabels[`${indexBy}`].structure.map(item => {
+        return item;
+      });
     }
-    //when cross-filtering "Most Requested" as index
-    else if (queryType === "Sessions" && crossFilter !== "") {
-      data = filterByDate(data, startDate, endDate);
-
+    if (crossFilter !== "") {
       data = removeMultiple(data);
-
-      dataStructure = getIndex(data, indexBy);
 
       return setCrossedItems(
         data,
@@ -72,33 +81,14 @@ const dataParse = (
         crossFilterQuery
       );
     } else {
-      if (queryType === "Users") {
-        dataStructure = graphLabels[`${indexBy}`].structure.map(item => {
-          return item;
-        });
-      }
-      if (crossFilter !== "") {
-        data = removeMultiple(data);
+      //when single filtering with index that is not "Most Requested"
+      data = removeMultiple(data);
 
-        return setCrossedItems(
-          data,
-          dataStructure,
-          crossFilter,
-          indexBy,
-          additionalFilter,
-          queryType,
-          crossFilterQuery
-        );
-      } else {
-        //when single filtering with index that is not "Most Requested"
-        data = removeMultiple(data);
-
-        // console.log(`dataparse after removeMultiple`, data);
-        return setItem(data, dataStructure, indexBy);
-      }
- 
+      // console.log(`dataparse after removeMultiple`, data);
+      return setItem(data, dataStructure, indexBy);
+    }
+  }
 };
-}
 // const filterByDate = (data, startDate, endDate) => {
 //   //console.log(`filterByDate`, data, startDate, endDate)
 //   startDate = startDate.replace(/-/g, "");
